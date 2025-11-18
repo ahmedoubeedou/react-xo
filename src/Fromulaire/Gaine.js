d37454ce3b4e87270c52b2f30bf883d95948e1a7
ahmedoubeedou <ahmedoumedbedou@gmail.com>
2025-11-18 09:26:07 +0000

    Add conditional message display in MessageGaine

--- src/Fromulaire/Gaine.js
+++ src/Fromulaire/Gaine.js
@@ -1,10 +1,11 @@
 import "./test.css"
 export default function MessageGaine({Message,namePlayer=""})
 {
+    const valuer = " triomphe avec intelligence";
     return(
         <div className="message-erure"> 
             <div className="message-erurre-background">
-                <h1  className="responsive-contenu" style={Message==="Magnifique ! Le joueur"?{color:"green"}:{color:"red"}}>{Message + "  "+namePlayer+" triomphe avec intelligence"}</h1>
+                <h1  className="responsive-contenu" style={Message==="Magnifique ! Le joueur"?{color:"green"}:{color:"red"}}>{Message + "  "+namePlayer+" "}{namePlayer.length>=2?valuer:""}</h1>
             </div>
         </div>
     );
